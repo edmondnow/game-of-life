520249c467208f1cefea57170994683204375bd8
edmondnow <edmond.bitay@gmail.com>
2018-05-10 16:03:55 +0200

    added game

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from 'redux-promise';
 
 
-import App from './components/app';
+//import App from './components/app';
 
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
   <div>
-     <App />
+     <div />
   </div>
 
   , document.querySelector('.container'));
